5e17c9dcb19196ce1d2dee9840b7ff487009357c
Halldór Eldjárn <halldor@eldjarn.net>
2019-02-19 12:35:18 +0000

    Added WIP S3 upload modal

---
+++ src/components/modals/providers/S3UploadModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <modal-inner aria-label="Upload">
+    <div class="modal__content">
+      <div class="modal__image">
+        
+      </div>
+    </div>
+    <div class="modal__button-bar">
+      <button class="button" @click="config.reject()">Cancel</button>
+      <button class="button button--resolve" @click="resolve()">Ok</button>
+    </div>
+  </modal-inner>
+</template>
+
+<script>
+import modalTemplate from '../common/modalTemplate';
+import store from '../../../store';
+import AWS from 'aws-sdk';
+
+export default modalTemplate({
+  data: () => ({
+    
+  }),
+  created() {
+    console.log("Here");
+  },
+  methods: {
+    resolve() {
+      
+    },
+  },
+});
+</script>
