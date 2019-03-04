be4b02a9755dd7309958932eec7e9133fae55e94
Halldór Eldjárn <halldor@eldjarn.net>
2019-03-04 20:13:35 +0000

    Working file upload with resize

--- server/assets.js
+++ server/assets.js
@@ -2,6 +2,8 @@ const AWS = require('aws-sdk');
 const request = require('request');
 const _ = require('lodash');
 const config = require('../config');
+const sharp = require('sharp');
+const filenamify = require('filenamify');
 
 AWS.config.update({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -62,30 +64,72 @@ exports.uploadPictures = (req, res) => {
         <Buffer ff d8 ff e2 0b f8 49 43 43 5f 50 52 4f 46 49 4c 45 00 01 01 00 00 0b e8 00 00 00 00 02 00 00 00 6d 6e 74 72 52 47 42 20 58 59 5a 20 07 d9 00 03 00 1b ... >,
        size: 375921 }
        */
-
   // TODO: Resizing https://github.com/lovell/sharp
+  const folderName = filenamify(req.body.folderName);
 
-  const upload = (file, folderName) => new Promise((resolve, reject) => {
-    var params = { Bucket: bucketPictures,
-      Key: folderName + "/" + new Date().getTime() + "_" + file.originalname,
-      Body: file.buffer }
-    s3.upload(params, function(err, data) {
+  const createSize = (file) => new Promise((resolve, reject) => {
+    // Returns a promise
+    console.log("Inside resize", file);
+    sharp(file.buffer).resize(file.w, file.w, {
+      fit: sharp.fit.inside,
+      withoutEnlargement: true,
+    })
+    .toFormat('jpeg')
+    .toBuffer()
+    .then(resizedBuffer => {
+      console.log("resizedBuffer", resizedBuffer);
+      file.resizedBuffer = resizedBuffer;
+      resolve(file);
+    })
+    .catch(err => { console.error(err); reject(err); });
+  });
+
+  const resize = (file) => {
+    const outputSizes = [{ name: "large", w: 1200 },
+    { name: "medium", w: 600 },
+    { name: "small", w: 300 } ];
+    const buffer = file.buffer;
+    const originalName = file.originalname;
+
+    return outputSizes.map(outputSize => {
+      outputSize.buffer = buffer;
+      outputSize.originalName = originalName;
+      return outputSize;
+    });
+
+    //Promise.all(files).then(resized => resolve(resized)).catch(err => reject(err));
+  };
+
+  const upload = (file) => new Promise((resolve, reject) => {
+    const fileName = folderName + "/" + file.name + "/" + new Date().getTime() + "_" + file.originalName;
+    const params = {
+      Bucket: bucketPictures,
+      Key: fileName,
+      Body: file.resizedBuffer, };
+
+    console.log("file: ", file);
+    console.log("params: ", params);
+
+    return s3.upload(params, (err, data) => {
       if(err) {
+        console.error(err);
         return reject(err);
       }
       resolve(data);
     });
   });
 
-  Promise.all(req.files.map(upload)).then(uploadResultArray => {
-    // const results = _.map(uploadResultArray, result => {
-    //   return _.pick(result, )
-    // });
+  Promise.all(req.files.map(resize)).then( files => {
+    const resizedFlat = _.flatten(files);
+    Promise.all(resizedFlat.map(createSize)).then( resizedFiles => {
+      console.log(resizedFiles);
+      Promise.all(resizedFlat.map(upload)).then( uploadResults => {
+        res.status(201).json({ message: uploadResults });
+      }).catch(err => res.status(500).json({ err: err }));
+    }).catch(err => res.status(500).json({ err: err }));
+  }).catch(err => res.status(500).json({ err: err }));
 
-    // TODO: HTTP stuff
-    res.status(201).json({ uploadedFiles: uploadResultArray });
-  }).catch(err => console.error(err));
-
-  upload.then( data => console.log(data) )
-  .catch( err => console.error(err) );
+  // Promise.all(req.files.map(resizeAndUpload)).then(uploadResultArray => {
+  //   res.status(201).json({ uploadedFiles: uploadResultArray });
+  // }).catch(err => res.status(500).json({ error: "Upload failed" }));
 }
